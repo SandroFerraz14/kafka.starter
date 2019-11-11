cb814891e76b77c2fb8858ba2a34c36912c9ede0
Sandro Ferraz <sandro.ferraz@ccg.pt>
2019-11-11 15:12:44 +0000

    create KProducer class with keys - each key goes always to same partition

---
+++ src/main/java/com/github/sandroferraz14/kafka/starter/KProducerKeys.java
@@ -0,0 +1,68 @@
+package com.github.sandroferraz14.kafka.starter;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class KProducerKeys {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final Logger logger = LoggerFactory.getLogger(KProducerKeys.class);
+
+        String bootstrapServer = "ec2-3-134-95-187.us-east-2.compute.amazonaws.com:9092";
+
+        //craete Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i=0; i<10; i++) {
+            //create a producer record
+            String topic = "first_topic";
+            String value = "hello topic " + Integer.toString(i);
+            String key = "id_" + Integer.toString(i);
+
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, key, value);
+
+            logger.info("Key: " + key); //log th key
+            //id_0 partition 1
+            //id_1 partition 0
+            //id_2 partition 2
+            //id_3 partition 0
+            //id_4 partition 2
+            //id_5 partition 2
+            //id_6 partition 0
+            //id_7 partition 2
+            //id_8 partition 1
+            //id_9 partition 2
+
+            //send data - asynchronous
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    //executes every time a record is successfully sent or exception is thrown
+                    if (e == null) {
+                        //the record was successfully sent
+                        logger.info("Received new metadata. \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
+                    } else {
+                        logger.error("Error while producing", e);
+                    }
+                }
+            }).get(); //block the .send() to make it synchronous - don't do this in production!
+        }
+        //flush data
+        producer.flush();
+        //flush and close producer
+        producer.close();
+    }
+}
