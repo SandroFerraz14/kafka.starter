22856e1e4999f0af676c57a1517dec574fe79bf9
Sandro Ferraz <sandro.ferraz@ccg.pt>
2019-11-13 18:57:31 +0000

    improve performance of consumer using batching

--- kafka.consumer.elasticsearch/src/main/java/elasticsearch/ElasticSearchConsumer.java
+++ kafka.consumer.elasticsearch/src/main/java/elasticsearch/ElasticSearchConsumer.java
@@ -12,8 +12,9 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.protocol.types.Field;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -23,7 +24,6 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.yaml.snakeyaml.events.Event;
 
 import java.io.IOException;
 import java.time.Duration;
@@ -68,7 +68,7 @@ public class ElasticSearchConsumer {
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); //read from beginning of topic
         //latest ---> read only new messages of topic
         properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false"); //disable auto commit of offsets
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "10");
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100");
 
         //create consumer
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
@@ -93,7 +93,11 @@ public class ElasticSearchConsumer {
         while(true){
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100)); //new in Kafka 2.0.0
 
-            logger.info("Received " + records.count() + " records");
+            Integer recordCount = records.count();
+            logger.info("Received " + recordCount + " records");
+
+            BulkRequest bulkRequest = new BulkRequest();
+
             for (ConsumerRecord<String, String> record : records){
                 //2 strategies to create ids
 
@@ -101,32 +105,33 @@ public class ElasticSearchConsumer {
                 //String id = record.topic() + "_" + record.partition() + "_" + record.offset();
 
                 //twitter feed specific id
-                String id = extractIdFromTweet(record.value());
+                try{
+                    String id = extractIdFromTweet(record.value());
 
-                //where we insert data into ElasticSearch
-                String jsonString = record.value();
+                    //where we insert data into ElasticSearch
+                    String jsonString = record.value();
 
-                IndexRequest indexRequest = new IndexRequest(
-                        "twitter",
-                        "tweets",
-                        id //this is to make our consumer idempotent, avoid duplicated data
-                ).source(jsonString, XContentType.JSON);
+                    IndexRequest indexRequest = new IndexRequest(
+                            "twitter",
+                            "tweets",
+                            id //this is to make our consumer idempotent, avoid duplicated data
+                    ).source(jsonString, XContentType.JSON);
 
-                IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-                logger.info(indexResponse.getId());
+                    bulkRequest.add(indexRequest); //we add to our bulk request (takes no time)
+                } catch (NullPointerException e){
+                    logger.warn("skipping bad data: " + record.value());
+                }
+            }
+            if (recordCount > 0){
+                BulkResponse bulkItemResponses = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+                logger.info("Committing offsets...");
+                consumer.commitSync();
+                logger.info("Offsets have been committed");
                 try {
-                    Thread.sleep(100); //introduce a small delay
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            }
-            logger.info("Committing offsets...");
-            consumer.commitSync();
-            logger.info("Offsets have been committed");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
         }
 
