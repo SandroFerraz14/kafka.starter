73d587e6784bb514d78d881d2618582dd0b978b8
Sandro Ferraz <sandro.ferraz@ccg.pt>
2019-11-13 17:44:36 +0000

    create a new package for consumer (elasticsearch) module and setup project

--- kafka.consumer.elasticsearch/src/main/java/elasticsearch/ElasticSearchConsumer.java
+++ kafka.consumer.elasticsearch/src/main/java/elasticsearch/ElasticSearchConsumer.java
@@ -1,4 +1,65 @@
 package elasticsearch;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 public class ElasticSearchConsumer {
+
+    public static RestHighLevelClient createClient(){
+        Dotenv dotenv = Dotenv.load();
+        String hostname = dotenv.get("ES_HOSTNAME");
+        String username = dotenv.get("ES_USERNAME");
+        String password = dotenv.get("ES_PASSWORD");
+
+        //don't do if you run a local ElasticSearch
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        RestClientBuilder builder = RestClient.builder(
+                new HttpHost(hostname, 443, "https"))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
+                        return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+
+                });
+        RestHighLevelClient client = new RestHighLevelClient(builder);
+        return client;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
+        RestHighLevelClient client = createClient();
+
+        String jsonString = "{ \"foo\": \"bar\" }";
+
+        IndexRequest indexRequest = new IndexRequest(
+                "twitter",
+                "tweets"
+        ).source(jsonString, XContentType.JSON);
+
+        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+        String id = indexResponse.getId();
+        logger.info(id);
+
+        //close the client gracefully
+        client.close();
+    }
 }
